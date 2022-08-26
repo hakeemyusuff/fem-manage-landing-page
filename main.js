788abb412138b84ab82abd61deefbe0470207c56
Yusuff Abdulhakeem <hakeemyusuff19@gmail.com>
2022-08-25 21:58:19 -0700

    version 1

--- main.js
+++ main.js
@@ -9,3 +9,34 @@ navToggle.addEventListener("click", () => {
   primaryNav.toggleAttribute("data-visible");
   primaryHeader.toggleAttribute("data-overlay");
 });
+
+/* const slider = new A11YSlider(document.querySelector('.slider'), {
+  adaptiveHeight: true,
+  dots: false
+}); */
+
+// core version + navigation, pagination modules:
+import Swiper, { Navigation, Pagination } from "swiper";
+// import Swiper and modules styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+// init Swiper:
+const swiper = new Swiper(".swiper", {
+  modules: [Navigation, Pagination],
+  slidesPerView: 3,
+  spaceBetween: 20,
+
+  // Pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
