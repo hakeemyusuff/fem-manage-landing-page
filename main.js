1bcd1b95716b4f5f1a6373c0ae9f80b2ce249352
Yusuff Abdulhakeem <hakeemyusuff19@gmail.com>
2022-08-27 20:58:22 -0700

    version 2

--- main.js
+++ main.js
@@ -18,9 +18,9 @@ navToggle.addEventListener("click", () => {
 // core version + navigation, pagination modules:
 import Swiper, { Navigation, Pagination } from "swiper";
 // import Swiper and modules styles
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
+import "./swiper/css";
+import "./swiper/css/navigation";
+import "./swiper/css/pagination";
 
 // init Swiper:
 const swiper = new Swiper(".swiper", {
